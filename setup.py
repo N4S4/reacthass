d717e12e00a15abc9183e1601e91a7ffd87845c4
Renato <33936751+N4S4@users.noreply.github.com>
2023-03-23 21:08:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reacthass',
+    version='0.1',
+    packages=['reacthass'],
+    url='https://github.com/N4S4/reacthass',
+    license='GPL-3.0 license',
+    author='Renato Visaggio',
+    author_email='',
+    description='Homeassistant API automation helper'
+)
