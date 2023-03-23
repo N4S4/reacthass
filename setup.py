bbe033c89f73d2a4ec912454f9083fd23847fa16
Renato <33936751+N4S4@users.noreply.github.com>
2023-03-23 21:20:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='reacthass',
-    version='0.1',
+    version='0.1.1',
     packages=['reacthass'],
     url='https://github.com/N4S4/reacthass',
     license='GPL-3.0 license',
+    description='Homeassistant API automation helper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Renato Visaggio',
-    author_email='',
-    description='Homeassistant API automation helper'
 )
