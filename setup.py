1e2273c60dc327108e882b06872e1a5f25fbd455
Renato <33936751+N4S4@users.noreply.github.com>
2023-03-23 21:31:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='reacthass',
-    version='0.1.1',
+    version='0.1.2',
     packages=['reacthass'],
     url='https://github.com/N4S4/reacthass',
     license='GPL-3.0 license',
